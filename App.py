d9b23b67b41017d0abe92e791e9d05803820c855
Nelson D'Silva <ndsilva822@gmail.com>
2019-10-12 09:30:50 -0700

    fixed swiping, added red flag input, needs scroll handling

--- App.py
+++ App.py
@@ -1,20 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import pytesseract
 import os
 from PIL import Image
-from selenium.webdriver.common.action_chains import ActionChains
 import re
 from selenium.common.exceptions import NoSuchElementException
 
-def facebookLogin(driver):
+def facebookLogin(driver, loginName, loginPassword):
     driver.get('http://www.facebook.com/')
     time.sleep(3)
     username = driver.find_element_by_css_selector("input[name=email")
-    username.send_keys('')
+    username.send_keys(loginName)
     password = driver.find_element_by_css_selector('input[name=pass]')
-    password.send_keys('')
+    password.send_keys(loginPassword)
     try:
         driver.find_element_by_css_selector('input[type=submit]').click()
     except:
@@ -30,22 +28,34 @@ def bumbleLogin(driver):
         NoSuchElementException
 
 def readProfile(driver):
-    actions = ActionChains(driver)
+    time.sleep(2)
     profileText = []
     for x in range(5):
         driver.save_screenshot('screen.png')
         profileText.append(pytesseract.image_to_string(Image.open('screen.png')))
         if os.path.exists('screen.png'):
             os.remove('screen.png')
-        actions.send_keys(Keys.ARROW_DOWN)
-    judgeProfile(profileText, driver)
+            driver.find_element_by_xpath("//svg[@style, 'transform: rotate(-90deg)')]").click()
+        time.sleep(1)
+    profileText = ''.join(profileText)
+    judgeProfile(profileText)
 
-def judgeProfile(profileText, driver):
-    ActionChains.send_keys(Keys.ARROW_LEFT) if (re.match('fluent in sarcasm',''.join(profileText))) else ActionChains.send_keys(Keys.ARROW_RIGHT)
+def buildRedFlagRegex(redFlag):
+    redFlag = redFlag.split(",")
+
+    return '^('+''.join(['(?!'+ x + ')' for x in redFlag])+'.)*$'
+
+def judgeProfile(profileText):
+    driver.find_element_by_xpath("//*[contains(@class, 'button--transparent')]").click() if \
+        (re.match(buildRedFlagRegex(redFlag), profileText)) else \
+        driver.find_element_by_xpath("//div[contains(@class, 'encounters-action--like')]").click()
 
 if __name__ == "__main__":
+    loginName = input("Your email/facebook login  :")
+    loginPassword = input("Your facebook password  :")
+    redFlag = input("what don't you like about people?  :")
     driver = webdriver.Chrome()
-    facebookLogin(driver)
+    facebookLogin(driver, loginName, loginPassword)
     bumbleLogin(driver)
     readProfile(driver)
     time.sleep(5)
