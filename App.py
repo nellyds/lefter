51ba7a9a269fe1c62967c3a10881585c90473b27
Nelson D'Silva <ndsilva822@gmail.com>
2019-10-15 15:14:59 -0700

    fixed loop, possibly

--- App.py
+++ App.py
@@ -5,6 +5,8 @@ import os
 from PIL import Image
 import re
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 def facebookLogin(driver, loginName, loginPassword):
     driver.get('http://www.facebook.com/')
@@ -17,7 +19,7 @@ def facebookLogin(driver, loginName, loginPassword):
         driver.find_element_by_css_selector('input[type=submit]').click()
     except:
         NoSuchElementException
-    time.sleep(5)
+        facebookLogin(driver, loginName, loginPassword)
 
 def bumbleLogin(driver):
     driver.get("https://www.bumble.com/get-started")
@@ -26,37 +28,54 @@ def bumbleLogin(driver):
         driver.find_element_by_xpath("//div[contains(@class, 'color-provider-facebook')]").click()
     except:
         NoSuchElementException
+        bumbleLogin(driver)
 
 def readProfile(driver):
-    time.sleep(2)
+    actions = ActionChains(driver)
+    time.sleep(5)
     profileText = []
     for x in range(5):
         driver.save_screenshot('screen.png')
         profileText.append(pytesseract.image_to_string(Image.open('screen.png')))
         if os.path.exists('screen.png'):
             os.remove('screen.png')
-            driver.find_element_by_xpath("//svg[@style, 'transform: rotate(-90deg)')]").click()
+        try:
+            profileWindow = driver.find_element_by_xpath("//div[contains(@class, 'encounters-action--like')]")
+            actions.move_to_element(profileWindow).send_keys(Keys.DOWN).perform()
+        except:
+            NoSuchElementException
+            driver.close()
         time.sleep(1)
     profileText = ''.join(profileText)
     judgeProfile(profileText)
 
 def buildRedFlagRegex(redFlag):
     redFlag = redFlag.split(",")
-
     return '^('+''.join(['(?!'+ x + ')' for x in redFlag])+'.)*$'
 
 def judgeProfile(profileText):
-    driver.find_element_by_xpath("//*[contains(@class, 'button--transparent')]").click() if \
+    driver.find_element_by_xpath("//*[contains(@class, 'encounters-action--like')]").click() if \
         (re.match(buildRedFlagRegex(redFlag), profileText)) else \
-        driver.find_element_by_xpath("//div[contains(@class, 'encounters-action--like')]").click()
+        driver.find_element_by_xpath("//div[contains(@class, 'encounters-action--dislike')]").click()
+
+
+def main_loop(driver):
+    try:
+        driver.find_element_by_xpath("//*[contains(text(), 'all caught up')]")
+        driver.close()
+    except:
+        readProfile(driver)
+
+
+
 
 if __name__ == "__main__":
-    loginName = input("Your email/facebook login  :")
-    loginPassword = input("Your facebook password  :")
-    redFlag = input("what don't you like about people?  :")
+    loginName = input("Your email/facebook login  :  ")
+    # loginPassword = input("Your facebook password  :  ")
+    redFlag = input("what don't you like about people?  :  ")
     driver = webdriver.Chrome()
-    facebookLogin(driver, loginName, loginPassword)
+    facebookLogin(driver, 'ndsilva822@gmail.com', 'ppfFrisky11')
     bumbleLogin(driver)
-    readProfile(driver)
-    time.sleep(5)
-    driver.close()
+    main_loop(driver)
+
+
